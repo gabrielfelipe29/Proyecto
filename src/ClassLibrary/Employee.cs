b6ad8eb8ca5f73eb479f22b1db2a2cf725d2e8aa
German Adjemian <GERMAN.ADJEMIAN@correo.ucu.edu.uy>
2022-09-24 17:03:50 -0300

    Employer y Employee

---
+++ src/ClassLibrary/Employee.cs
@@ -0,0 +1,33 @@
+using System;
+namespace ClassLibrary;
+public class Employer
+{
+    public string Name { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+    private string Passaword { get; set; }
+    private int PhoneNumber {get; set; }
+    private Location Location {get; set; }
+    private List<Request> RequestList {get; set; }
+    public void Register()
+    {
+        
+    }
+    public void Login()
+    {
+        
+    }
+    public Service CreateService()
+    {
+        
+    }
+    public double GetRatingAverage()
+    {
+        
+    }
+    public void AddRequest()
+    {
+
+    }
+
+}
